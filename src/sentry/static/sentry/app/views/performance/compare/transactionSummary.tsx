1568543b79057287e132a057c7129119adec051f
Alberto Leal <mail4alberto@gmail.com>
2020-07-24 14:28:19 -0400

    header

--- src/sentry/static/sentry/app/views/performance/compare/transactionSummary.tsx
+++ src/sentry/static/sentry/app/views/performance/compare/transactionSummary.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import {Event} from 'app/types';
+import {t} from 'app/locale';
 import space from 'app/styles/space';
+import {Event} from 'app/types';
+import {getHumanDuration, parseTrace} from 'app/components/events/interfaces/spans/utils';
 
 import {isTransactionEvent} from './utils';
 
@@ -20,21 +22,60 @@ class TransactionSummary extends React.Component<Props> {
       return null;
     }
 
-    // const baselineTraceContext = getTraceContext(baselineEvent);
-    // const regressionTraceContext = getTraceContext(regressionEvent);
+    const baselineTrace = parseTrace(baselineEvent);
+    const regressionTrace = parseTrace(regressionEvent);
+
+    const baselineDuration = Math.abs(
+      baselineTrace.traceStartTimestamp - baselineTrace.traceEndTimestamp
+    );
+    const regressionDuration = Math.abs(
+      regressionTrace.traceStartTimestamp - regressionTrace.traceEndTimestamp
+    );
 
     return (
-      <div>
+      <Container>
         <EventRow>
-          <Baseline /> baseline event: {baselineEvent.eventID}
+          <Baseline />
+          <EventRowContent>
+            <Content>
+              <ContentTitle>{t('Baseline Event')}</ContentTitle>
+              <EventId>{`ID: ${shortEventId(baselineEvent.eventID)}`}</EventId>
+            </Content>
+            <TimeDuration>
+              <span>{getHumanDuration(baselineDuration)}</span>
+            </TimeDuration>
+          </EventRowContent>
         </EventRow>
         <EventRow>
-          <Regression /> regressive event: {regressionEvent.eventID}
+          <Regression />
+          <EventRowContent>
+            <Content>
+              <ContentTitle>{t('Regressive Event')}</ContentTitle>
+              <EventId>{`ID: ${shortEventId(regressionEvent.eventID)}`}</EventId>
+            </Content>
+            <TimeDuration>
+              <span>{getHumanDuration(regressionDuration)}</span>
+            </TimeDuration>
+          </EventRowContent>
         </EventRow>
-      </div>
+      </Container>
     );
   }
 }
+
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+
+  justify-content: space-between;
+  align-content: space-between;
+
+  padding-bottom: ${space(1)};
+
+  > * + * {
+    margin-top: ${space(0.75)};
+  }
+`;
 
 const EventRow = styled('div')`
   display: flex;
@@ -42,7 +83,7 @@ const EventRow = styled('div')`
 
 const Baseline = styled('div')`
   background-color: ${p => p.theme.purple300};
-  height: 40px;
+  height: 100%;
   width: 4px;
 
   margin-right: ${space(1)};
@@ -50,10 +91,44 @@ const Baseline = styled('div')`
 
 const Regression = styled('div')`
   background-color: ${p => p.theme.gray700};
-  height: 40px;
+  height: 100%;
   width: 4px;
 
   margin-right: ${space(1)};
 `;
+
+const EventRowContent = styled('div')`
+  flex-grow: 1;
+  display: flex;
+`;
+
+const TimeDuration = styled('div')`
+  display: flex;
+  align-items: center;
+
+  font-size: ${p => p.theme.headerFontSize};
+  line-height: 1.2;
+
+  margin-left: ${space(1)};
+`;
+
+const Content = styled('div')`
+  flex-grow: 1;
+  width: 150px;
+
+  font-size: ${p => p.theme.fontSizeMedium};
+`;
+
+const ContentTitle = styled('div')`
+  font-weight: 600;
+`;
+
+const EventId = styled('div')`
+  color: ${p => p.theme.gray500};
+`;
+
+function shortEventId(value: string): string {
+  return value.substring(0, 8);
+}
 
 export default TransactionSummary;
